1e4a7548b757e33629a0ef83fa1d70102291f983
unknown <sebastian-olthoff@gmx.de>
2024-07-31 14:53:21 +0200

    updated index.js with esm modules+added esm to package.json

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-import react from 'react';
-import { Sequelize } from 'sequelize';
+import express from 'express';
+import './db/index.js';
+import morgan from 'morgan';
+import cors from 'cors';
+import userRoutes from './router/users.js';
+import notesRoutes from './router/notes.js';
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use('/users', userRoutes);
+app.use('/notes', notesRoutes);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
